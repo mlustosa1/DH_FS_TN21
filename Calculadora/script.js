553a6b1920ed3d000b83001a02d4d693e8c6a726
Marcelo Rodrigues <marcelorodrigues2005@gmail.com>
2022-09-28 15:27:55 -0300

    implementando funcionalidades da calculadora com js

---
+++ Calculadora/script.js
@@ -0,0 +1,23 @@
+// Autores: Pedro Henrique / Marcelo José
+
+let btns = document.querySelectorAll(".btn");
+let visor = document.querySelector(".visor input");
+for (let x = 0; x < btns.length; x++){
+let btn = btns[x]
+if ("1234567890-+*/()".includes(btn.innerText)){
+    btn.addEventListener("click", function() {
+    visor.value += btn.innerText;
+    })}
+else if ('<' == btn.innerText){
+    btn.addEventListener("click", function() {
+    visor.value = visor.value.slice(0, - 1);})
+    }
+else if ('C' == btn.innerText){
+    btn.addEventListener("click", function(){
+    visor.value = "";
+    })}
+else if ('=' == btn.innerText){
+    btn.addEventListener("click", function(){
+    visor.value = eval(visor.value)
+    })}
+}
